d67372c0c7a8c8bb49b0146d4e89cf58e066c731
Pedro Cella <pedrovscella@gmail.com>
2023-01-13 15:22:56 -0300

    Adicionando classe Board
    Co-authored-by: Andrebenf <andrebenf@gmail.com>
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>

---
+++ Board.py
@@ -0,0 +1,20 @@
+import chess
+import chess.svg
+from pathlib import Path
+
+
+class Board:
+
+    def __init__(self):
+        pass
+    
+    def get_new_board():
+    # TODO implementar método que vai gerar um novo tabuleiro no ínicio de um novo jogo
+        return 10
+        
+
+    def update_board(self, currentFen):
+        board = chess.Board(fen=currentFen)
+        with open(Path(__file__).parent.joinpath("game.svg"), mode="w") as img:
+            img.write(chess.svg.board(board, size=350))
+  
