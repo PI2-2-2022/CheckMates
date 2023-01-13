2559787b5e0298bbc96f83f56001244d92d46b05
Pedro Cella <pedrovscella@gmail.com>
2023-01-13 16:15:46 -0300

    Adicionando arquivo de constantes
    Co-authored-by: Andrebenf <andrebenf@gmail.com>
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>

---
+++ constants.py
@@ -0,0 +1,16 @@
+import os
+
+STOCKFISH_PATH = os.getenv("STOCKFISH_PATH")
+STARTING_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+FILLED_ROW = [1, 1, 1, 1, 1, 1, 1, 1]
+EMPTY_ROW = [0, 0, 0, 0, 0, 0, 0, 0]
+INITIAL_BOARD_BIT = [
+    FILLED_ROW,
+    FILLED_ROW,
+    EMPTY_ROW,
+    EMPTY_ROW,
+    EMPTY_ROW,
+    EMPTY_ROW,
+    FILLED_ROW,
+    FILLED_ROW,
+]
