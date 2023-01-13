f88eb70d248a4fac91802e06949bd7d3cec08725
Pedro Cella <pedrovscella@gmail.com>
2023-01-13 16:15:00 -0300

    Adicionando classe Validation
    Co-authored-by: Andrebenf <andrebenf@gmail.com>
    Co-authored-by: NilvanPeres <nilvanperes@gmail.com>

---
+++ Validation.py
@@ -0,0 +1,9 @@
+class Validation:
+    # Classe responsável por administrar as regras e violações do jogo
+
+    def __init__(self) -> None:
+        pass    
+        
+    def is_game_over(self): # VALIDATION
+    # TODO verificar checkmate, empate, vitoria, etc...
+        return False
