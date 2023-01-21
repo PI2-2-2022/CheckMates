1dc42962f9b29d22fafc6c52823e3af967b31ac2
Pedro Cella <pedrovscella@gmail.com>
2023-01-21 11:47:38 -0300

    Implementando funcoes get_color e get_level

--- Interface.py
+++ Interface.py
@@ -16,43 +16,29 @@ class Interface:
     def __init__(self):
         pass
 
-    def get_level(): # GUI
-    # TODO pegar o nivel do display de niveis
-        return 10
+    def get_level(self): # GUI
+        level = input("Selecione em qual dificuldade você deseja jogar (0-20): ")
+        return int(level)
 
-    def get_color():
-    # TODO pegar com qual peça o jogador gostaria de jogar
-        return 10
+    def get_color(self):
+        color = input("Selecione com qual cor deseja jogar (w para jogar com as brancas ou b para jogar com as pretas): ")
+        return color
 
-    def start_game(self, stockfish: Stockfish, currentFen, currentBitBoard): # 
+    def start_game(self, stockfish: Stockfish, currentFen, currentBitBoard, color):
         if currentFen == None:
             currentFen = STARTING_FEN
-        while not validation.is_game_over():
-            # TODO descomentar quando o recebimento da bit board estiver funcionando
-            # targetBoard = get_next_board()
-            # move = get_move(targetBoard, currentBitBoard)
-
-            move = input('Insira sua jogada(ex.: e2e4): ')
-            # print('Movimento usuario:', move)
-
+        if color == 'b':
             stockfish.set_fen_position(currentFen)
-            # print('FEN inicio rodada:', stockfish.get_fen_position())
-
-            # TODO fazer as validacoes do movimento
-            stockfish.make_moves_from_current_position([move])
-            # print('FEN apos movimento usuario:', stockfish.get_fen_position())
-
             bestMove = stockfish.get_best_move()
-            # print('Movimento da IA:', bestMove)
             stockfish.make_moves_from_current_position([bestMove])
-            # print('FEN apos movimento IA:', stockfish.get_fen_position())
-
+            currentFen = stockfish.get_fen_position()
+        while not validation.is_game_over():
+            move = input('Insira sua jogada(ex.: e2e4): ')
+            stockfish.make_moves_from_current_position([move])
+            bestMove = stockfish.get_best_move()
+            stockfish.make_moves_from_current_position([bestMove])
             currentFen = stockfish.get_fen_position()
             matrix = communication.fen_to_matrix(currentFen)
-            # print('\n\nmatriz: ')
-            # print_matrix(matrix)
             currentBitBoard = communication.matrix_to_bitboard(matrix)
-            # print('\n\nbit board: ')
-            # print_matrix(currentBitBoard)
             communication.send_bit_board(currentBitBoard)
             board.update_board(currentFen)
