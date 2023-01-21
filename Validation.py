612486c0423c3694f4e8aa91b81d49ba109652cc
Pedro Cella <pedrovscella@gmail.com>
2023-01-21 18:10:35 -0300

    Adicionando is_checkmate e is_valid_move

--- Validation.py
+++ Validation.py
@@ -1,3 +1,7 @@
+from constants import STOCKFISH_PATH
+from stockfish import Stockfish
+import chess
+
 class Validation:
     # Classe responsável por administrar as regras e violações do jogo
 
@@ -7,3 +11,15 @@ class Validation:
     def is_game_over(self): # VALIDATION
     # TODO verificar checkmate, empate, vitoria, etc...
         return False
+
+    def is_checkmate(self, fen: str) -> bool:
+        board = chess.Board(fen)
+        return board.is_checkmate()
+
+    def is_valid_move(self, move: str, currentFen: str) -> bool:
+        board = chess.Board(currentFen)
+        try:
+            board.push_uci(move)
+            return True
+        except ValueError:
+            return False
